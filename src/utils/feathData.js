602e117cb8ad196426baa5356f1a69172e81610c
hainl2000 <haingluong2000@gmail.com>
2022-02-07 09:28:20 +0700

    merge

--- src/utils/feathData.js
+++ src/utils/feathData.js
@@ -1,43 +1,43 @@
-import axios from "axios";
+import axios from "axios"
 
-const apiUrl = "https://ltct-sp19-api.herokuapp.com";
+const apiUrl = "https://ltct-sp19-api.herokuapp.com"
 
 export const getData = async (url) => {
-  const res = await axios.get(`${apiUrl}/api/${url}`);
-  const data = res.data;
-  return data;
-};
+  const res = await axios.get(`${apiUrl}/api/${url}`)
+  const data = res.data
+  return data
+}
 
 export const postData = async (url, post) => {
-  const res = await axios.post(`${apiUrl}/api/${url}`, post);
-  const data = res.data;
-  return data;
-};
+  const res = await axios.post(`${apiUrl}/api/${url}`, post)
+  const data = res.data
+  return data
+}
 
 export const putData = async (url, post) => {
-  const res = await axios.put(`${apiUrl}/api/${url}`, post);
+  const res = await axios.put(`${apiUrl}/api/${url}`, post)
 
-  const data = res.data;
-  return data;
-};
+  const data = res.data
+  return data
+}
 
 export const patchData = async (url, post) => {
-  console.log(post);
+  console.log(post)
   const res = await fetch(`${apiUrl}/api/${url}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(post),
-  });
+  })
 
-  const data = await res.json();
-  return data;
-};
+  const data = await res.json()
+  return data
+}
 
 export const deleteData = async (url, dt) => {
   const res = await axios.delete(`${apiUrl}/api/${url}`,{
     data: dt
-  });
-  return res;
-};
+  })
+  return res
+}
